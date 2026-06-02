cea806b6187efa6822747f946f28204b26eabdf4
FarisSF97 <alfarisi97@outlook.com>
2026-06-02 08:16:13 +0700

    fix forgot pass

--- common/routes.js
+++ common/routes.js
@@ -61,14 +61,6 @@ router.post("/process_forgot_password", async (req, res) => {
   auth.processForgotPassword(req, res);
 });
 
-router.get("/reset-password/:token", async (req, res) => {
-  auth.resetPassword(req, res);
-});
-
-router.post("/process_reset_password", async (req, res) => {
-  auth.processResetPassword(req, res);
-});
-
 router.get("/logout", (req, res) => {
   auth.logout(req, res);
 });
